c3854eaafa887be066bd53a05e587eb98d962d08
yuanjunliang <1610712512@qq.com>
2023-04-24 14:48:19 +0800

    add deploy config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import { HardhatUserConfig } from 'hardhat/config'
 import 'hardhat-deploy'
 import 'hardhat-deploy-ethers'
+import '@nomiclabs/hardhat-etherscan'
 
 const accounts = process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : ['']
 
@@ -18,11 +19,22 @@ const config: HardhatUserConfig = {
   namedAccounts: {
     deployer: process.env.DEPLOYER || ''
   },
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN || '',
+      bscTestnet: process.env.BSCSCAN || ''
+    }
+  },
   networks: {
     hardhat: {},
     sepolia: {
       url: 'https://rpc.sepolia.org',
       chainId: 11155111,
+      accounts
+    },
+    'bsc-testnet': {
+      url: 'https://bsc-testnet.public.blastapi.io',
+      chainId: 97,
       accounts
     }
   }
